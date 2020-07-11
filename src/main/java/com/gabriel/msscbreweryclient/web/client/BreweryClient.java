2c776155586f51046d9b2fdcf85a315edcf51be3
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-07-11 15:26:42 +0200

    add savenewBeer and updateBeer client

--- src/main/java/com/gabriel/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/com/gabriel/msscbreweryclient/web/client/BreweryClient.java
@@ -4,9 +4,11 @@ import com.gabriel.msscbreweryclient.web.model.BeerDto;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Setter
@@ -25,4 +27,13 @@ public class BreweryClient {
     public BeerDto getBeerById(UUID uuid){
         return restTemplate.getForObject(apihost+BEER_PATH_V1+uuid.toString(), BeerDto.class);
     }
+
+    public URI saveNewBeer(BeerDto beerDto){
+        return restTemplate.postForLocation(apihost+BEER_PATH_V1, beerDto);
+    }
+
+    public void updateBeer(UUID uuid, BeerDto beerDto){
+        restTemplate.put(apihost+BEER_PATH_V1+uuid.toString(), beerDto);
+    }
+
 }
--- src/test/java/com/gabriel/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/com/gabriel/msscbreweryclient/web/client/BreweryClientTest.java
@@ -4,7 +4,9 @@ import com.gabriel.msscbreweryclient.web.model.BeerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,4 +24,23 @@ class BreweryClientTest {
 
         assertNotNull(dto);
     }
+
+    @Test
+    void saveNewBeer() {
+
+        BeerDto beer = BeerDto.builder().beerName("tyskie").build();
+
+        URI uri = client.saveNewBeer(beer);
+
+        System.out.println(uri.toString());
+        assertNotNull(uri);
+    }
+
+    @Test
+    void updateBeer() {
+
+        BeerDto beer = BeerDto.builder().beerName("tyskie").build();
+        client.updateBeer(UUID.randomUUID(), beer);
+    }
+
 }
